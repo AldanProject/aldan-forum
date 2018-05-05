af10a96a2de2e29c71b4d60ef7f22c0cc52c94aa
unknown <alejandro-hdez115@outlook.com>
2018-05-05 13:58:44 -0500

    Add background image

--- source/index.php
+++ source/index.php
@@ -14,42 +14,42 @@
   <body>
     <!-- Navbar -->
     <?php include_once("lib/inc/navbar.php"); ?>
-
     <!-- Content -->
-    <div id="main-container" class="main-container">
-      <?php
-      if(isset($_GET['user']))
-      {
-        include_once("lib/user.php");
-      }
-      else if(isset($_GET['edit-profile']))
-      {
-        //include_once("lib/config/edit-profile.php");
-      }
-      else if(isset($_GET['new-post']))
-      {
-        //include_once("lib/post/new-post.php");
-      }
-      else if(isset($_GET['post']))
-      {
-        include_once("lib/forum/post-content.php");
-      }
-      else if(isset($_GET['forum']))
-      {
-        include_once("lib/forum/forum-content.php");
-      }
-      else if(isset($_GET['new-forum']))
-      {
-        include_once("lib/forum/new-forum.php");
-      }
-      else
-      {
-        print("<script>document.title = 'Inicio | Foro de Aldan Project';</script>");
-        include_once("lib/forums.php");
-      }
-      ?>
+    <div class="backgroungImage">
+      <div id="main-container" class="main-container">
+        <?php
+        if(isset($_GET['user']))
+        {
+          include_once("lib/user.php");
+        }
+        else if(isset($_GET['edit-profile']))
+        {
+          //include_once("lib/config/edit-profile.php");
+        }
+        else if(isset($_GET['new-post']))
+        {
+          //include_once("lib/post/new-post.php");
+        }
+        else if(isset($_GET['post']))
+        {
+          include_once("lib/forum/post-content.php");
+        }
+        else if(isset($_GET['forum']))
+        {
+          include_once("lib/forum/forum-content.php");
+        }
+        else if(isset($_GET['new-forum']))
+        {
+          include_once("lib/forum/new-forum.php");
+        }
+        else
+        {
+          print("<script>document.title = 'Inicio | Foro de Aldan Project';</script>");
+          include_once("lib/forums.php");
+        }
+        ?>
+      </div>
     </div>
-
     <!-- Footer -->
     <?php include_once("lib/inc/footer.php"); ?>
   </body>
